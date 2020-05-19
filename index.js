fed37b3a817bf3d12d3624bf452268431bb71873
sadjad <sghgh1996@yahoo.com>
2020-05-19 19:42:13 +0200

    feat: painting part

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ var sliderArrowRight = document.getElementsByClassName('slider-arrow-right')[0];
 var sliderArrowLeft = document.getElementsByClassName('slider-arrow-left')[0];
 var selectedColorBox = document.getElementsByClassName('selected-color-box')[0];
 
+const sizeInputs = document.querySelectorAll('.input-box .input input');
+
+var paintingCanvas = document.querySelector('.painting-canvas');
+var paintingCanvasCtx = paintingCanvas.getContext('2d');
+
+paintingCanvas.addEventListener('mousedown', function(e) {
+  paintingCanvasMouseDown(paintingCanvas, slider.value, e);
+});
+
+updatePaintingBoard();
+
 fixSlider(slider.value);
 
 slider.oninput = function() {
-  console.log('change');
-  console.log(this.value);
   fixSlider(this.value)
 }
 
@@ -67,12 +76,50 @@ function HSLToHex(h, s, l) {
   return "#" + r + g + b;
 }
 
-const sizeInputs = document.querySelectorAll('.input-box .input input');
-
 sizeInputs[0].oninput = function(e) {
   sizeInputs[1].value = e.target.value;
+  updatePaintingBoard();
 }
 
 sizeInputs[1].oninput = function(e) {
   sizeInputs[0].value = e.target.value;
+  updatePaintingBoard();
+}
+
+function updatePaintingBoard () {
+  paintingCanvasCtx.clearRect(0, 0, paintingCanvas.width, paintingCanvas.height);
+
+  paintingCanvasCtx.beginPath();
+  paintingCanvasCtx.strokeStyle = '#d7d7d7';
+  const paintCellNumbers = parseInt(sizeInputs[0].value);
+  const paintCellSize = 400 / paintCellNumbers;
+
+  for (let i = 0; i < paintCellNumbers; i++) {
+    
+    for (let j = 0; j < paintCellNumbers; j++) {
+      paintingCanvasCtx.rect(i * paintCellSize, j * paintCellSize, paintCellSize, paintCellSize);
+    }
+  }
+  paintingCanvasCtx.stroke();
+}
+
+
+function getMousePosition(canvas, e) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top
+  };
+}
+
+function paintingCanvasMouseDown(canvas, colorValue, e) {
+  const paintCellNumbers = parseInt(sizeInputs[0].value);
+  const paintCellSize = 400 / paintCellNumbers;
+
+  const mousePosition = getMousePosition(canvas, e);
+  const rowIndex = Math.ceil(mousePosition.x / paintCellSize);
+  const columnIndex = Math.ceil(mousePosition.y / paintCellSize);
+  
+  paintingCanvasCtx.fillStyle = HSLToHex(colorValue, 100, 50);
+  paintingCanvasCtx.fillRect((rowIndex - 1) * paintCellSize, (columnIndex - 1) * paintCellSize, paintCellSize, paintCellSize);
 }
